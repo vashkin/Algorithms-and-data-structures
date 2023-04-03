ac55b55db644e0f6ea604ca8385d3b855cdd66c5
Vashkin.m <93059581+vashkin@users.noreply.github.com>
2023-04-03 18:52:36 +0400

    Update Seminar-2-pyramidal-sorting-algorithm.java
    Homework for the seminar 2

--- Seminar-2-pyramidal-sorting-algorithm.java
+++ Seminar-2-pyramidal-sorting-algorithm.java
@@ -1,1 +1,58 @@
 /*Реализовать алгоритм пирамидальной сортировки (сортировка кучей).*/
+import java.util.Arrays;
+
+public class HeapSort {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 3, 1, 8, 6, 4, 2, 7};
+        heapSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // Построение кучи (перегруппировка массива)
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Один за другим извлекаем элементы из кучи
+        for (int i = n - 1; i >= 0; i--) {
+            // Перемещаем текущий корень в конец
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Вызываем heapify на уменьшенной куче
+            heapify(arr, i, 0);
+        }
+    }
+
+    // Функция для перегруппировки элементов в поддереве с корнем i
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int l = 2 * i + 1; // Левый дочерний элемент
+        int r = 2 * i + 2; // Правый дочерний элемент
+
+        // Если левый дочерний элемент больше корня
+        if (l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+
+        // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+        if (r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+
+        // Если самый большой элемент не корень
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Рекурсивно перегруппировать затронутую поддерево
+            heapify(arr, n, largest);
+        }
+    }
+}
